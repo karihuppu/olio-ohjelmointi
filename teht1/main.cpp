289174aecf406a275673f56a3fc3a2945a5351b9
Marco <kinnunen.marco0@gmail.com>
2024-03-10 19:44:07 +0200

    teht1 on tehty

---
+++ teht1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a,b,c;
+    float avg;
+    cout<<"Anna 1. luku"<<endl;
+    cin>>a;
+    cout<<"Anna 2. luku"<<endl;
+    cin>>b;
+    cout<<"Anna 3. luku"<<endl;
+    cin>>c;
+    avg=(a+b+c)/3;
+    cout<<"keskiarvo on "<<avg<<endl;
+    return 0;
+}
