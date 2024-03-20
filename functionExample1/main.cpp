2a2e22f3d0171936d014e6f00e44b152b2f2a91b
Marco <kinnunen.marco0@gmail.com>
2024-03-20 12:40:05 +0200

    functionExample2 aloitettu

--- functionExample1/main.cpp
+++ functionExample1/main.cpp
@@ -1,9 +1,22 @@
+//Tee funktio, joka ottaa vastaan 2 kokonaislukua
+//ja palauttaa niiden summan
 #include <iostream>
 
 using namespace std;
+int calculateSum(int a, int b);
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int x,y,sum;
+    cout << "Anna kokonaisluku" << endl;
+    cin>>x;
+    cout << "Anna toinen kokonaisluku"<<endl;
+    cin>>y;
+    sum=calculateSum(x,y);
+    cout<<"Lukujen summa on "<<sum<<endl;
     return 0;
+}
+
+int calculateSum(int a, int b){
+    return a+b;
 }
