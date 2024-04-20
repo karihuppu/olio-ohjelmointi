126d108f0cb761510d11a08ccb3bcbc4ab38d022
Marco <kinnunen.marco0@gmail.com>
2024-04-20 18:08:32 +0300

    animal luokka ok

---
+++ compositionExample/animal.h
@@ -0,0 +1,20 @@
+#ifndef ANIMAL_H
+#define ANIMAL_H
+#include<iostream>
+using namespace std;
+
+class Animal
+{
+private:
+         //string color;
+public:
+    Animal();
+    ~Animal();
+    string getColor() const;
+    void setColor(const string &newColor);
+protected:
+    string color;
+
+};
+
+#endif // ANIMAL_H
